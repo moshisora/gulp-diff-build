f9b085620a1f283140943402388caf8304030e6d
moshisora <moshisora990@gmail.com>
2019-11-08 18:47:34 +0900

    format code

--- test/hash_test.js
+++ test/hash_test.js
@@ -1,80 +1,87 @@
-'use strict';
+'use strict'
 
-var fs = require('fs'),
-    assert = require('assert'),
+const assert = require('assert'),
     gulp = require('gulp'),
     diff = require('../index.js'),
     concatStream = require('concat-stream'),
-    rimraf = require('rimraf');
+    rimraf = require('rimraf')
 
-const capture = require('./captureStream.js')( process.stdout );
-capture.reset();
+const capture = require('./captureStream.js')(process.stdout)
+capture.reset()
 
 describe('hash', function () {
     before(function (callback) {
-        rimraf('.gulp/gulp-diff-build', callback);
-    });
+        rimraf('.gulp/gulp-diff-build', callback)
+    })
 
     it('run as default hash', function (callback) {
-        capture.on();
+        capture.on()
         gulp.src('test/src/*.js')
             .pipe(diff())
             .pipe(concatStream(function (buf) {
-                let out = capture.get();
-                capture.off(false);
-                assert(out.has(capture.messages.changes));
-                assert.equal(3, buf.length);
-                callback();
+                const out = capture.get()
+                capture.off(false)
+                assert(out.has(capture.messages.changes))
+                assert.equal(3, buf.length)
+                callback()
             }))
-            .on('unpipe',function(){capture.reset()});
-    });
+            .on('unpipe', function () {
+                capture.reset()
+            })
+    })
 
     it('run as another hash', function (callback) {
-        capture.on();
+        capture.on()
         gulp.src('test/src/*.js')
             .pipe(diff({
                 hash: 'another'
             }))
             .pipe(concatStream(function (buf) {
-                let out = capture.get();
-                capture.off(false);
-                assert(out.has(capture.messages.changes));
-                assert.equal(3, buf.length);
-                callback();
+                const out = capture.get()
+                capture.off(false)
+                assert(out.has(capture.messages.changes))
+                assert.equal(3, buf.length)
+                callback()
             }))
-            .on('unpipe',function(){capture.reset()});
-    });
+            .on('unpipe', function () {
+                capture.reset()
+            })
+    })
 
     it('clear another hash', function (callback) {
-        capture.on();
+        capture.on()
         gulp.src('test/src/*.js')
             .pipe(diff({
                 clear: true,
                 hash: 'another'
             }))
             .pipe(concatStream(function (buf) {
-                let out = capture.get();
-                capture.off(false);
-                assert(out.has(capture.messages.flushing));
-                assert(out.has(capture.messages.flushingCompleted));
-                assert(out.has(capture.messages.changes));
-                assert.equal(3, buf.length);
-                callback();
+                const out = capture.get()
+                capture.off(false)
+                assert(out.has(capture.messages.flushing))
+                assert(out.has(capture.messages.flushingCompleted))
+                assert(out.has(capture.messages.changes))
+                assert.equal(3, buf.length)
+                callback()
             }))
-            .on('unpipe',function(){capture.reset()});
-    });
+            .on('unpipe', function () {
+                capture.reset()
+            })
+    })
 
     it('default hash is not cleard', function (callback) {
-        capture.on();
+        capture.on()
         gulp.src('test/src/*.js')
             .pipe(diff())
             .pipe(concatStream(function (buf) {
-                let out = capture.get();
-                capture.off(false);
-                assert(out.has(capture.messages.noChanges));
-                assert.equal(0, buf.length);
-                callback();
+                const out = capture.get()
+                capture.off(false)
+                assert(out.has(capture.messages.noChanges))
+                assert.equal(0, buf.length)
+                callback()
             }))
-            .on('unpipe',function(){capture.reset()});
-    });
-});
+            .on('unpipe', function () {
+                capture.reset()
+            })
+    })
+})
